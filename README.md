a580cc18f524ce74c522f4f53ccbc7dd18f0c11d
YaelGF <1719110736@utectulancingo.edu.mx>
2023-03-06 22:34:55 -0600

    readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # DevCofeePWA
+
 Pwa 
+
+# How to Install
+
+
+``` shell
+git clone https://github.com/YaelGF/DevCofeePWA.git
+```
+
+# Run the project
+
+## elementos.html
+![elementos.html](/assets/Elementos.png)
+
+## Tabla.html
+![Tabla.html](/assets/Tabla.png)
+
+## Formularios.html
+![Formularios.html](/assets/Formularios.png)
+
+## Formulario-CSS.html
+![FormulariosCSS.html](/assets/FormulariosCSS.png)
+![Data Picker](/assets/FormulariosCSSDatapicker.png)
+
+# More Information
+
+![HTML Labels](/assets/HTMLLabel.png)
+![Css Labels](/assets/CSSLabel.png)
+# License
+[Apache License 2.0](https://github.com/YaelGF/DevCofeePWA/blob/main/LICENSE)
---
+++ js/app.js
@@ -0,0 +1,56 @@
+const container = document.querySelector(".container");
+const coffees = [
+  {
+    name: "Perspiciatis",
+    image: "images/cafe.jpg"
+  },
+  {
+    name: "Voluptatem",
+    image: "images/cafe.jpg"  },
+  {
+    name: "Explicabo",
+    image: "images/cafe.jpg"  },
+  {
+    name: "Rchitecto",
+    image: "images/cafe.jpg"  },
+  {
+    name: " Beatae",
+    image: "images/cafe.jpg"  },
+  {
+    name: " Vitae",
+    image: "images/cafe.jpg"  },
+  {
+    name: "Inventore",
+    image: "images/cafe.jpg"  },
+  {
+    name: "Veritatis",
+    image: "images/cafe.jpg"  },
+  {
+    name: "Accusantium",
+    image: "images/cafe.jpg"  }
+];
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  );
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
